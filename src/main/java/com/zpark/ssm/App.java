c7b4422a692f6d8f6d8c6649bcfc030ec4fadefd
lxy <lxy@163.com>
2023-05-06 07:59:55 +0800

    First Commit

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -3,6 +3,5 @@ package com.zpark.ssm;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("Hello Git,Second!!");
     }
 }
